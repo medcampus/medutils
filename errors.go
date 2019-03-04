2792b12fd9d9a3fad5309927b59decc9becad7cd
medCampus <venkatesh@rapidrevise.co.in>
2019-03-05 03:08:08 +0530

    updated errors with jwt errors

--- errors.go
+++ errors.go
@@ -4,9 +4,15 @@ import "errors"
 
 var (
 
-	// ErrInvalidObjId
+	// ErrInvalidObjId indicates invalid mongo object id
 	ErrInvalidObjId = errors.New("invalid object id")
 
-	// ErrNotFound
+	// ErrNotFound indicates document not found
 	ErrNotFound = errors.New("mongo document not found")
+
+	// ErrReadClaims indicates error while reading claims fro token
+	ErrReadClaims = errors.New("error reading jwt claims")
+
+	// ErrTokenInvalid indicates
+	ErrTokenInvalid = errors.New("invalid JWT token")
 )
--- utils.go
+++ utils.go
@@ -1,9 +1,7 @@
 package medutils
 
 import (
-	"context"
 	"fmt"
-	"github.com/sirupsen/logrus"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"gopkg.in/mgo.v2/bson"
@@ -11,10 +9,6 @@ import (
 
 func ConvertToGrpcErr(code codes.Code, message string) error {
 	return status.Errorf(code, message)
-}
-
-func GrpcLogError(context context.Context, err error) {
-	logrus.Errorf("method = %v, Error = %v", context.Value("method"), err)
 }
 
 func ValidObjectId(objectId ...string) (error, bool) {
