ae4b0341b7828b233c74c5243441061241895f78
medCampus <venkatesh@rapidrevise.co.in>
2019-03-06 00:29:54 +0530

    updated with grpc gracefull shutdown

---
+++ shutdown_grpc.go
@@ -0,0 +1,28 @@
+package medutils
+
+import (
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"net"
+	"os"
+	"os/signal"
+)
+
+func waitForInterrupt() {
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, os.Interrupt)
+
+	<-c
+}
+
+func Shutdown(s *grpc.Server, l net.Listener) {
+	// wait for interupt signal
+	waitForInterrupt()
+
+	logrus.Infof("Stopping gRPC server: %v", s.GetServiceInfo())
+	s.GracefulStop()
+
+	logrus.Infof("Closing listener at: %v", l.Addr())
+	l.Close()
+}
