fd117613fda871669154c50925e3b52a9a3fa271
medCampus <venkatesh@rapidrevise.co.in>
2019-03-06 15:30:56 +0530

    updated database connection

--- db.go
+++ db.go
@@ -11,8 +11,8 @@ import (
 )
 
 var (
-	session *mgo.Session
-	once sync.Once
+	session                *mgo.Session
+	once                   sync.Once
 	dbName, collectionName string
 )
 
@@ -23,13 +23,16 @@ type Handler struct {
 // create root DB Session
 func createDBSession() (session *mgo.Session, err error) {
 	mongoUrl := viper.GetString("mongo.host")
+
 	session, err = mgo.Dial(mongoUrl)
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, fmt.Sprintf("error creating root mongo session %s\n", err.Error()))
 	}
 	log.Infof("Created ROOT Database Session from %s", mongoUrl)
+
 	dbName = viper.GetString("mongo.dbName")
 	collectionName = viper.GetString("mongo.collectionName")
+
 	return session, nil
 }
 
