591154ba3d76d5ce584c37ddec4cc9875c234509
medCampus <venkatesh@rapidrevise.co.in>
2019-03-14 12:56:48 +0530

    removed convert_to_grpc_error

--- valid_object_id.go
+++ valid_object_id.go
@@ -3,13 +3,14 @@ package medutils
 import (
 	"fmt"
 	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 	"gopkg.in/mgo.v2/bson"
 )
 
 func ValidObjectId(objectId ...string) (error, bool) {
 	for _, object := range objectId {
 		if !bson.IsObjectIdHex(object) {
-			return ConvertToGrpcErr(codes.InvalidArgument, fmt.Sprintf("Invalid ObjectId %v", object)), false
+			return status.Errorf(codes.InvalidArgument, fmt.Sprintf("Invalid ObjectId %v", object)), false
 		}
 	}
 	return nil, true
