b73ff6fbec0006ef63e49de7b7328162c9cc9c24
medCampus <venkatesh@rapidrevise.co.in>
2019-04-18 15:34:06 +0530

    added convert_to_lower

---
+++ conver_to_lower.go
@@ -0,0 +1,13 @@
+package medutils
+
+import "strings"
+
+func ConvertToLower(tags []string) ([]string) {
+	var tagsLower []string
+	for _, tag := range tags{
+		tag = strings.ToLower(strings.TrimSpace(tag))
+		tagsLower = append(tagsLower, tag)
+	}
+
+	return tagsLower
+}
