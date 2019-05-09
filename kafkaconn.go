a982d32373bc741f7e00460cd60f02643765e6ae
ssatturi <srikanth.satturi@gmail.com>
2019-05-09 23:07:36 +0530

    kafka connection added

--- kafkaconn.go
+++ kafkaconn.go
@@ -6,7 +6,7 @@ import (
 	"github.com/spf13/viper"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-		"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 
 	"gopkg.in/mgo.v2"
 	"sync"
@@ -23,11 +23,11 @@ func createKafkaConnection() (kafkaconn *kafka.Producer, err error) {
 	kafkaconn, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": kafkaHostsUrl})
 
 	if err != nil {
-		return nil, status.Errorf(codes.Internal, fmt.Sprintf("error creating root mongo session %s\n", err.Error()))
+		return nil, status.Errorf(codes.Internal, fmt.Sprintf("error creating kafka connection %s\n", err.Error()))
 	}
 	log.Infof("Created Kafka Connection from %s", kafkaHostsUrl)
 
-	return session, nil
+	return kafkaconn, nil
 }
 
 
@@ -35,15 +35,15 @@ func GetKafkaSession() *kafka.Producer {
 
 	once.Do(func() {
 		var err error
-		session, err = createKafkaConnection()
+		kafkaconn, err = createKafkaConnection()
 		if err != nil {
 			log.Fatalf("Unable to create Kafka session, Error %v", err)
 		}
 	})
 
-	return session.Copy()
+	return kafkaconn.Copy()
 }
 
 func CloseRootSession() {
-	session.Close()
+	kafkaconn.Close()
 }
