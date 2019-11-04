9fa2c0afdd1ff7b4b613833d296900adc3060734
medCampus <venkatesh@rapidrevise.co.in>
2019-11-04 16:14:54 +0530

    added gocache lib

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ require (
 	github.com/gin-contrib/sse v0.0.0-20190301062529-5545eab6dad3 // indirect
 	github.com/gin-gonic/gin v1.3.0
 	github.com/golang/protobuf v1.3.0
+	github.com/gomodule/redigo v2.0.0+incompatible // indirect
 	github.com/google/uuid v1.1.1
 	github.com/mattn/go-isatty v0.0.6 // indirect
 	github.com/pkg/errors v0.8.1
---
+++ gocache/connection.go
@@ -0,0 +1,53 @@
+package gocache
+
+import (
+	"github.com/gomodule/redigo/redis"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+	"sync"
+)
+
+var (
+	pool *redis.Pool
+	once sync.Once
+)
+
+type Client struct {
+	Conn redis.Conn
+}
+
+func initPool() *redis.Pool {
+	return &redis.Pool{
+		MaxIdle:   80,
+		MaxActive: 12000,
+		Dial: func() (redis.Conn, error) {
+			conn, err := redis.Dial("tcp", viper.GetString("redis.host"))
+			if err != nil {
+				log.Printf("ERROR: fail init redis pool: %s", err.Error())
+				os.Exit(1)
+			}
+			return conn, err
+		},
+	}
+}
+
+func GetClient() *Client {
+	once.Do(func() {
+		pool = initPool()
+	})
+
+	return &Client{Conn: pool.Get()}
+}
+
+func (c *Client) Close() {
+	defer c.Conn.Close()
+}
+
+func Ping(conn redis.Conn) {
+	_, err := redis.String(conn.Do("PING"))
+	if err != nil {
+		log.Printf("ERROR: fail ping redis conn: %s", err.Error())
+		os.Exit(1)
+	}
+}
