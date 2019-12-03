a454b7b4f635dc50a06f1788835e100aeb439c4c
medCampus <venkatesh@rapidrevise.co.in>
2019-12-03 15:19:57 +0530

    added mcrand lib

---
+++ mcrand/rand.go
@@ -0,0 +1,59 @@
+package mcrand
+
+import (
+	rand1 "crypto/rand"
+	"encoding/base64"
+	"github.com/google/uuid"
+	"math/rand"
+)
+
+// Returns an int >= min, < max
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+// Generate a random string of A-Z chars with len = l
+func randomString(len int) string {
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		bytes[i] = byte(randomInt(65, 90))
+	}
+	return string(bytes)
+}
+
+func GetRandomInt(min, max int) int {
+	return randomInt(min, max)
+}
+
+func GetRandomString(len int) string {
+	return randomString(len)
+}
+
+// GenerateRandomBytes returns securely generated random bytes.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomBytes(n int) ([]byte, error) {
+	b := make([]byte, n)
+	_, err := rand1.Read(b)
+	// Note that err == nil only if we read len(b) bytes.
+	if err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+// GenerateRandomString returns a URL-safe, base64 encoded
+// securely generated random string.
+// It will return an error if the system's secure random
+// number generator fails to function correctly, in which
+// case the caller should not continue.
+func GenerateRandomString(s int) (string, error) {
+	b, err := GenerateRandomBytes(s)
+	return base64.URLEncoding.EncodeToString(b), err
+}
+
+func GetUUID() uuid.UUID {
+	return uuid.New()
+}
