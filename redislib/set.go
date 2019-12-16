9a099a38dbd31b5afae0870b7d7eb29f7440c150
medCampus <venkatesh@rapidrevise.co.in>
2019-12-16 15:52:09 +0530

    added without exp methods

--- redislib/set.go
+++ redislib/set.go
@@ -5,7 +5,7 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func (c *Client) SetByte(key string, value []byte, durationInSeconds int64) error {
+func (c *Client) SetByteWithExp(key string, value []byte, durationInSeconds int64) error {
 	_, err := c.Conn.Do("SETEX", key, durationInSeconds, string(value))
 	if err != nil {
 		return status.Errorf(codes.Internal, "ERROR: fail set key %s, val %s, error %s", key, value, err.Error())
@@ -14,7 +14,7 @@ func (c *Client) SetByte(key string, value []byte, durationInSeconds int64) erro
 	return nil
 }
 
-func (c *Client) SetString(key string, value string, durationInSeconds int64) error {
+func (c *Client) SetStringWithExp(key string, value string, durationInSeconds int64) error {
 	_, err := c.Conn.Do("SETEX", key, durationInSeconds, value)
 	if err != nil {
 		return status.Errorf(codes.Internal, "ERROR: fail set key %s, val %s, error %s", key, value, err.Error())
@@ -23,7 +23,7 @@ func (c *Client) SetString(key string, value string, durationInSeconds int64) er
 	return nil
 }
 
-func (c *Client) SetInt32(key string, value int32, durationInSeconds int64) error {
+func (c *Client) SetIntWithExp(key string, value int, durationInSeconds int64) error {
 	_, err := c.Conn.Do("SETEX", key, durationInSeconds, value)
 	if err != nil {
 		return status.Errorf(codes.Internal, "ERROR: fail set key %s, val %s, error %s", key, value, err.Error())
@@ -32,8 +32,44 @@ func (c *Client) SetInt32(key string, value int32, durationInSeconds int64) erro
 	return nil
 }
 
-func (c *Client) SetInt64(key string, value int64, durationInSeconds int64) error {
+func (c *Client) SetInt64WithExp(key string, value int64, durationInSeconds int64) error {
 	_, err := c.Conn.Do("SETEX", key, durationInSeconds, value)
+	if err != nil {
+		return status.Errorf(codes.Internal, "ERROR: fail set key %s, val %s, error %s", key, value, err.Error())
+	}
+
+	return nil
+}
+
+func (c *Client) SetByte(key string, value []byte) error {
+	_, err := c.Conn.Do("SET", key, string(value))
+	if err != nil {
+		return status.Errorf(codes.Internal, "ERROR: fail set key %s, val %s, error %s", key, value, err.Error())
+	}
+
+	return nil
+}
+
+func (c *Client) SetString(key string, value string) error {
+	_, err := c.Conn.Do("SET", key, value)
+	if err != nil {
+		return status.Errorf(codes.Internal, "ERROR: fail set key %s, val %s, error %s", key, value, err.Error())
+	}
+
+	return nil
+}
+
+func (c *Client) SetInt(key string, value int) error {
+	_, err := c.Conn.Do("SET", key, value)
+	if err != nil {
+		return status.Errorf(codes.Internal, "ERROR: fail set key %s, val %s, error %s", key, value, err.Error())
+	}
+
+	return nil
+}
+
+func (c *Client) SetInt64(key string, value int64) error {
+	_, err := c.Conn.Do("SET", key, value)
 	if err != nil {
 		return status.Errorf(codes.Internal, "ERROR: fail set key %s, val %s, error %s", key, value, err.Error())
 	}
